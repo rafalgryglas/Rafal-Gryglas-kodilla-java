e4bcd1c693242c69a174510e8785d5b0a089eb5c
rafalgryglas <rafal.gryglas@gmail.com>
2018-09-08 19:34:17 +0200

    Testing 6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,34 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+        System.out.println("Test - pierwszy test jednostkowy: dodawanie");
+
+        Calculator calculator = new Calculator(15, 10);
+        int testAdd = calculator.getA() + calculator.getB();
+        if (testAdd == calculator.add(15, 10)) {
+            System.out.println("Test - add a to b: OK");
+        } else {
+            System.out.println("Error test add a to b");
+        }
+        System.out.println("Test - drugi test jednostkowy: odejmowanie");
+        int testSubstract = calculator.getB() - calculator.getA();
+        if (testSubstract == calculator.substract(15, 10)) {
+            System.out.println("Test - Substract b from a: OK");
+        } else {
+            System.out.println("Error test substract b from a");
+        }
     }
 }
