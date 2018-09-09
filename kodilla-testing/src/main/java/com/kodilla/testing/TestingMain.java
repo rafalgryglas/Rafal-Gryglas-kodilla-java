5797ba3916b76905556c3695b99e4cc3ec5b9ce6
rafalgryglas <rafal.gryglas@gmail.com>
2018-09-09 20:50:14 +0200

    refactor

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -5,15 +5,7 @@ import com.kodilla.testing.user.SimpleUser;
 
 public class TestingMain {
     public static void main(String[] args) {
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
 
-        String result = simpleUser.getUsername();
-
-        if (result.equals("theForumUser")) {
-            System.out.println("test OK");
-        } else {
-            System.out.println("Error!");
-        }
         System.out.println("Test - pierwszy test jednostkowy: dodawanie");
 
         Calculator calculator = new Calculator(15, 10);
